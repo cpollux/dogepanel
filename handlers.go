569ac5fbf917fbf65eeeec76e0c8a71b41dea0f0
Arno Meuter <meuter.arno@gmail.com>
2017-12-22 11:46:52 +0100

    Draft

---
+++ handlers.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"text/template"
+)
+
+var loginT = template.Must(template.New("login").ParseFiles("templates/base.html", "templates/login.html"))
+var panelT = template.Must(template.New("panel").ParseFiles("templates/base.html", "templates/panel.html"))
+
+type Page struct {
+
+	Title string
+}
+
+
+func ViewLoginHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	p := Page{"DogePanel | Login"}
+	loginT.ExecuteTemplate(w, "base", p)
+}
+
+func SubmitLoginHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome! 3\n")
+}
+
+func ViewPanelHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	p := Page{"DogePanel"}
+	panelT.ExecuteTemplate(w, "base", p)
+}
+
+func ViewDataHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome! 4\n")
+}
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+	"os"
+    "path/filepath"
+)
+
+func main() {
+
+	ex, err := os.Executable() // get directory the binary is placed in
+    if err != nil {
+        panic(err)
+    }
+    dirname := filepath.Dir(ex) 
+
+	router := httprouter.New()
+	router.GET("/", ViewPanelHandler)
+	router.GET("/login", ViewLoginHandler)
+	router.POST("/login", SubmitLoginHandler)
+	router.GET("/data", ViewDataHandler)
+	router.ServeFiles("/static/*filepath", http.Dir(filepath.Join(dirname, "static/")))
+
+	log.Fatal(http.ListenAndServe(":52525", router))
+}
